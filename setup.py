ed2de8b35cbbce22de1f054033a76b3df15c4947
builder07 <47144637+builder07@users.noreply.github.com>
2019-04-22 15:05:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import setup_translate
 setup(name = 'enigma2-plugin-systemplugins-swapmanager',
 		version='1.0',
 		author='www.xtrend-united.de',
-		author_email='www.xtrend-united.de',
 		package_dir = {'SystemPlugins.SwapManager': 'src'},
 		packages=['SystemPlugins.SwapManager'],
 		description = 'do swapping, like you want',
